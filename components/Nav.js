5713f3f380732ed6ea502fa0331393af7a95fa82
Sriraj Bura <sriraj.bura16@gmail.com>
2021-09-21 13:00:48 -0400

    added nav bar

--- components/Nav.js
+++ components/Nav.js
@@ -1,21 +1,36 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from '../styles/Nav.module.css';
 
 export default function Nav({ children, home }) {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <h1>Sriraj Bura</h1>
-      <div className="page-links">
-        {/* <Link>
-          <a>Projects</a>
-        </Link>
-        <Link>
-          <a>Skills</a>
-        </Link>
-        <Link>
-          <a>About</a>
-        </Link> */}
-      </div>
+      <ul className={styles.nav_links}>
+        <li>
+          {' '}
+          <Link href="/">
+            <a className={styles.nav_link}>Projects</a>
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link href="/">
+            <a className={styles.nav_link}>Skills</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className={styles.nav_link}>About</a>
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link href="/">
+            <a className={styles.nav_link_resume}>Resume</a>
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
