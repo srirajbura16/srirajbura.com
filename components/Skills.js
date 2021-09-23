ea7055d370fcf408f66dead42db839351b0331b8
Sriraj Bura <sriraj.bura16@gmail.com>
2021-09-23 16:53:36 -0400

    small fixes

--- components/Skills.js
+++ components/Skills.js
@@ -3,9 +3,9 @@ import styles from '../styles/Layout.module.css';
 export default function Skills() {
   return (
     <div className={styles.container}>
-      <span className="note">
+      <div className="note">
         Note: The order is proficient to least proficient.
-      </span>
+      </div>
     </div>
   );
 }
