f2961926029575660838126ab4c91188e88af292
Sriraj Bura <sriraj.bura16@gmail.com>
2021-10-26 21:53:48 -0400

    navbar sticky

--- components/Nav.js
+++ components/Nav.js
@@ -5,7 +5,7 @@ export default function Nav() {
   return (
     <nav className={styles.nav}>
       <div className={styles.nav_items}>
-        <h1>Sriraj Bura</h1>
+        <h1 className={styles.logo}>Sriraj Bura</h1>
         <ul className={styles.nav_links}>
           <li>
             {' '}
