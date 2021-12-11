20f2af092c1747360c48573c08321b036de4abeb
Sriraj Bura <sriraj.bura16@gmail.com>
2021-12-11 09:42:28 -0500

    added css variables

--- components/Projects.js
+++ components/Projects.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styles from '../styles/Projects.module.css';
 import Link from 'next/link';
 import projects_data from '../lib/projects';
+import { FaGithub } from 'react-icons/fa';
 
 export default function Projects() {
   return (
@@ -24,6 +25,14 @@ export default function Projects() {
               <p className={styles.project_description}>
                 {project.small_description}
               </p>
+              {/* <div className={styles.project_links}>
+                <a href={`${project.site}`} target="_blank" rel="noreferrer">
+                  Live
+                </a>
+                <a href={`${project.github}`} target="_blank" rel="noreferrer">
+                  <FaGithub className={styles.social_icon} />
+                </a>
+              </div> */}
               <Link href={'projects/' + project.url_name}>
                 <a className={styles.read_more}>Read More</a>
               </Link>
