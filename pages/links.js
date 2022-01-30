ff87246166e3d9b64a2cf8b11b6436d7ddcda1fe
Sriraj Bura <sriraj.bura16@gmail.com>
2022-01-30 14:26:27 -0500

    added social icons

--- pages/links.js
+++ pages/links.js
@@ -2,29 +2,37 @@ import React from 'react';
 import styles from '../styles/Links.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
 
 export default function Links() {
   return (
     <div className={styles.container}>
-      <div className={styles.center}>
-        <div className={styles.profile_pic}>
-          <Image
-            src="/images/me.jpg"
-            alt="Picture of Sriraj Bura"
-            className={styles.img}
-            width={250}
-            height={250}
-          />
+      <div className={styles.profile_pic}>
+        <Image
+          src="/images/me.jpg"
+          alt="Picture of Sriraj Bura"
+          className={styles.img}
+          width={250}
+          height={250}
+        />
+      </div>
+      <div className={styles.links}>
+        <div className={styles.link}>
+          <FaTwitter className={`${styles.social_icon} ${styles.twitter}`} />
+          <Link href="">
+            <a>Twitter</a>
+          </Link>
         </div>
-        <div className={styles.links}>
+        <div className={styles.link}>
+          <FaGithub className={`${styles.social_icon} ${styles.github}`} />
           <Link href="">
-            <a className={styles.link}>Twitter</a>
+            <a>Github</a>
           </Link>
+        </div>
+        <div className={styles.link}>
+          <FaLinkedin className={`${styles.social_icon} ${styles.linkedin}`} />
           <Link href="">
-            <a className={styles.link}>Github</a>
-          </Link>
-          <Link href="">
-            <a className={styles.link}>Linkedin</a>
+            <a>Linkedin</a>
           </Link>
         </div>
       </div>
